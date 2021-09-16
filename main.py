9051442b09335e8e15d5da08b5da6a7538af20d4
Shawn Patel <shawnpatel@berkeley.edu>
2021-09-15 22:57:49 -0700

    python pipeline to update firestore with json

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+import firebase_admin
+from firebase_admin import firestore
+
+def main():
+    default_app = firebase_admin.initialize_app()
+
+    db = firestore.client()
+    plants_ref = db.collection("settings").document("plants")
+
+    with open("plants.json") as file:
+        data = json.load(file)
+        plants_ref.set(data)
+
+if __name__ == "__main__":
+    main()
