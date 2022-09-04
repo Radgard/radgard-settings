76f00b0f8d42ae6c2c567f23551674524b6eb474
Shawn Patel <shawnpatel@berkeley.edu>
2022-09-03 19:22:22 -0700

    update plants json

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     default_app = firebase_admin.initialize_app()
 
     db = firestore.client()
-    plants_ref = db.collection("settings").document("plants")
+    plants_ref = db.collection("laraCloudSettings").document("plants")
 
     with open("plants.json") as file:
         data = json.load(file)
